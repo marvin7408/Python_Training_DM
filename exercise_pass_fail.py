49edb41a4c9bde4f9f700933323178c1a335cde9
marvin7408 <m.tuinfort@gmail.com>
2024-04-12 15:51:24 +0200

    Added comments in the code

--- exercise_pass_fail.py
+++ exercise_pass_fail.py
@@ -2,8 +2,10 @@
 If it is 60 or higher, print "Pass", else print "Fail
 """
 
+#Variable score with input number between 0 and 100
 score = int(input("Enter a number between 0 and 100: "))
 
+#If statement to check if the entered number is equel or greater then 60
 if score >= 60:
     print("Pass")
 
